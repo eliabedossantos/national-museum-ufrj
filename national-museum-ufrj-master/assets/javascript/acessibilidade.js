73df32e4b6046b2b011577177d0bfa85a5f31e49
Eliabe dos Santos Oliveira <72847444+eliabedossantos@users.noreply.github.com>
2021-09-27 12:12:04 -0300

    modifica acessibilidade

---
+++ national-museum-ufrj-master/assets/javascript/acessibilidade.js
@@ -0,0 +1,63 @@
+function reply_click(clicked_id)
+  {
+    var className = $("body").attr("class")
+    if(className != clicked_id){
+    $("body").removeClass();
+    }
+    $("body" ).toggleClass(clicked_id);
+  }
+
+function fonte(tam_fonte) {
+
+	var elemento = $(".acessibilidade");
+	var fonte = parseInt(elemento.css('font-size'));
+
+	var body = $("body");
+	const fonteNormal = parseInt(body.css('font-size'));
+
+
+	if (tam_fonte == 'a' && fonte<20) {
+		fonte = fonte+2;
+	}
+	if (tam_fonte == 'd' && fonte>=16){
+		fonte = fonte-2;
+	}
+	if (tam_fonte == 'n'){
+		fonte = fonteNormal;
+	}
+
+	elemento.css("fontSize", fonte);
+	
+}
+var x = 0;
+function zoom(zoom_page) {
+
+	var elemento = $(".acessibilidade");
+	var zoom = parseInt(elemento.css('zoom'));
+
+	var body = $("body");
+	const zoomNormal = parseInt(body.css('zoom'));
+
+    
+	if (zoom_page == 'g') {
+        if(x<3){
+        x++;
+		zoom = 2*zoom;
+        }
+        
+	}
+	if (zoom_page == 'h'){
+        if(x > 0){
+		zoom = zoom/2;
+        x--;
+        }
+        
+	}
+	if (zoom_page == 'n'){
+		zoom = zoomNormal;
+	}
+
+	elemento.css("zoom", zoom);
+	
+}
+
